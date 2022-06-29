cdcacbff0a88136615dbc85c0dd87684df630fd7
Charles Opute Odili <chaluwa@gmail.com>
2022-06-29 11:11:31 +0000

    partition users by repo username

--- index.js
+++ index.js
@@ -47,32 +47,69 @@ const tastToChallengeName = (t) => {
     return `Challenge-0${n}`;
 };
 
-const reportATask = async (language, task, server) => {
+const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const shards = {
+    '0-4': 'A-E',
+    '5-9': 'F-J',
+    '10-14': 'K-O',
+    '15-19': 'P-T',
+    '20-25': 'U-Z'
+};
+
+const ownerToSheetPartition = (owner) => {
+    const initial = owner.charAt(0).toLowerCase();
+
+    let index = alphabets.indexOf(initial);
+    if (index === -1) index = 25;
+
+    const key = Object.keys(shards).find(k => {
+        const [start, end] = k.split('-');
+        return index >= parseInt(start, 10) && index <= parseInt(end, 10);
+    });
+
+    return shards[key];
+};
+
+const reportATask = async (language, task, opts) => {
     const stats = getStatsFor(language, task);
+    const { token, server, sheetid } = opts;
 
     const { repo, owner } = context.repo;
-    const report = {
+    const data = {
       repo,
       owner,
       ...stats,
       language,
-      source: 'unit-tests',
+      source: 'gha-jest-tests',
       type: tastToChallengeName(task)
     };
 
-    
-    await axios.post(`${server}/api/entry-tests`, { report });
+    const sheet = ownerToSheetPartition(owner);
+    await axios.post(`${server}/${sheet}`, data, {
+        headers: {
+            "X-Spreadsheet-Id": `${sheetid}`,
+            "Authorization": `Bearer ${token}`
+        }
+    });
 };
 
 const run = async () => {
   try {
-    const allTasks = core.getInput('challenge').split(/;\s*/);
+    const token = core.getInput('token');
     const language = core.getInput('lang');
     const server = core.getInput('server');
+    const sheetid = core.getInput('sheetid');
 
-    const reportingStack = allTasks.reduce(async (job, task) => {
-        await job;
-        reportATask(language, task, server)
+    const allTasks = core.getInput('challenge').split(/;\s*/);
+
+    const reportingStack = allTasks.reduce(async (previous, task) => {
+        return previous.then(
+            () => reportATask(language, task, {
+                token,
+                server,
+                sheetid
+            })
+        );
     }, Promise.resolve());
 
     await reportingStack;
